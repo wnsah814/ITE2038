480ce1eaa4ced9d7831b5d1dadabb385b8d73d71
2021092379 <2021092379@hanyang.ac.kr>
2022-11-18 12:29:48 +0900

    proj2: given codes

---
+++ project2/src/main.c
@@ -0,0 +1,43 @@
+#include "bpt.h"
+
+int main(){
+    int64_t input;
+    char instruction;
+    char buf[120];
+    char *result;
+    open_table("test.db");
+    while(scanf("%c", &instruction) != EOF){
+        switch(instruction){
+            case 'i':
+                scanf("%ld %s", &input, buf);
+                db_insert(input, buf);
+                break;
+            case 'f':
+                scanf("%ld", &input);
+                result = db_find(input);
+                if (result) {
+                    printf("Key: %ld, Value: %s\n", input, result);
+                }
+                else
+                    printf("Not Exists\n");
+
+                fflush(stdout);
+                break;
+            case 'd':
+                scanf("%ld", &input);
+                db_delete(input);
+                break;
+            case 'q':
+                while (getchar() != (int)'\n');
+                return EXIT_SUCCESS;
+                break;   
+
+        }
+        while (getchar() != (int)'\n');
+    }
+    printf("\n");
+    return 0;
+}
+
+
+
